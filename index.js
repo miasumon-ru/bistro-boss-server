d136cd48108fb27bdb204d5c5876097031001f42
miasumon <miasumon.ru@gmail.com>
2024-05-22 16:30:27 +0600

    api created for deleting of the carts

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k8vw6eq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -62,6 +62,26 @@ async function run() {
       const result = await cartCollection.find(query).toArray()
 
       res.send(result)
+    })
+
+    
+
+    app.delete('/carts/:id', async(req, res)=> {
+
+      const id = req.params.id
+
+      const query = {
+        _id : new ObjectId(id)
+      }
+
+
+      const result = await cartCollection.deleteOne(query)
+
+      res.send(result)
+
+
+
+      
     })
 
 
