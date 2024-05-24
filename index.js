f6012870d64f2a0a016cbb1dbaf51c3bd84862fd
miasumon <miasumon.ru@gmail.com>
2024-05-24 16:57:12 +0600

    user related api created and json webtoken installed

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express()
 require('dotenv').config()
 const cors = require('cors')
 
+const jwt = require('jsonwebtoken');
+
 const port = process.env.PORT || 5000
 
 // middleware
@@ -38,13 +40,111 @@ async function run() {
     const userCollection = client.db('bistroDB').collection("users")
 
 
+    // verify token
+
+    const verifyToken = (req, res, next) => {
+
+      // console.log("inside verify token: ", req.headers.authorization)
+
+      if(!req.headers.authorization){
+        return res.status(401).send({message : "unauthorized access"})
+      }
+
+      const token = req.headers.authorization.split(' ')[1]
+      
+
+      console.log('inside ', token)
+
+      next()
+    }
+
+    // jwt related api
+
+    app.post('/jwt', async(req, res)=> {
+
+      const user = req.body
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn : '1h'})
+
+      res.send({token})
+
+
+
+    })
+
+
     // users related api
 
     app.post('/users', async(req, res)=> {
 
       const user = req.body
 
+    
+      
+
+      // insert email if user does not exist 
+      const query = {
+        email : user.email
+      }
+
+      const existingUser = await userCollection.findOne(query)
+
+  
+
+      if(existingUser){
+
+        return res.send({message : "user already exists"})
+      }
+
       const result = await userCollection.insertOne(user)
+
+      res.send(result)
+
+
+
+    })
+
+    // get the users
+
+    app.get('/users', verifyToken, async(req, res)=> {
+
+    
+      const result = await userCollection.find().toArray()
+
+      res.send(result)
+    })
+
+    // delete the user
+
+    app.delete("/users/:id", async(req, res)=> {
+
+      const id = req.params.id
+
+      
+
+      const result = await userCollection.deleteOne(query)
+
+      res.send(result)
+
+
+
+    })
+
+    // patch the user
+
+    app.patch('/users/admin/:id', async(req, res)=> {
+
+      const id = req.params.id
+
+      const filter = {_id : new ObjectId(id)}
+
+      const updateDoc = {
+        $set: {
+          role : "admin"
+        },
+      };
+
+      const result =  await userCollection.updateOne(filter, updateDoc)
 
       res.send(result)
 
